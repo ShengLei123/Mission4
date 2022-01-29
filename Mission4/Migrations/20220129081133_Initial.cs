4612dfe1254b06145015a75228968c6193ad6378
ShengLei123 <starkray666666@gmail.com>
2022-01-29 01:13:32 -0700

    Add project files.

---
+++ Mission4/Migrations/20220129081133_Initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission4.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: true),
+                    Rating = table.Column<string>(nullable: true),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.MovieID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Action/Adventure", "dfds", true, null, null, "G", "sasa sa", 2121 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "Action/Adventure", "ewc edfw", false, null, null, "PG-13", "sasawqw", 2433 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Family", "some one", false, null, null, "PG", "some some", 2011 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+        }
+    }
+}
---
+++ Mission4/Models/MovieApplication.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class MovieApplication: DbContext
+    {
+        public MovieApplication (DbContextOptions<MovieApplication> options): base (options)
+        {
+
+        }
+        public DbSet<MovieResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieResponse>().HasData(
+                new MovieResponse
+                {
+                    MovieID = 1,
+                    Category = "Action/Adventure",
+                    Title = "sasa sa",
+                    Year = 2121,
+                    Director = "dfds",
+                    Rating = "G",
+                    Edited = true
+                },
+                new MovieResponse
+                {
+                    MovieID = 2,
+                    Category = "Action/Adventure",
+                    Title = "sasawqw",
+                    Year = 2433,
+                    Director = "ewc edfw",
+                    Rating = "PG-13",
+                    Edited = false
+                },
+                new MovieResponse
+                {
+                    MovieID = 3,
+                    Category = "Family",
+                    Title = "some some",
+                    Year = 2011,
+                    Director = "some one",
+                    Rating = "PG",
+                    Edited = false
+                }
+            );
+        }
+    }
+}
